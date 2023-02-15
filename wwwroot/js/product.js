ab504551ce61f733af053b76d83a33fa232f2851
Giuseppe-lore <loregiuseppe0@gmail.com>
2023-02-15 17:45:06 +0100

    Added edit and delete button in product List

--- wwwroot/js/product.js
+++ wwwroot/js/product.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url": "/Admin/Product/GetAll" 
+            "url": "/Admin/Product/GetAll"
         },
         "columns": [
             { "data": "title", "width": "15%" },
@@ -15,6 +15,23 @@ function loadDataTable() {
             { "data": "price", "width": "15%" },
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-20 text-center">
+                            <a href="/Admin/Product/Upsert?id=${data}"
+                               class="btn btn-primary mx-2">
+                               <i class="bi bi-pencil-square"></i>
+                            </a>
+                        
+                            <a class="btn btn-danger mx-2">
+                               <i class="bi bi-trash3-fill"></i>
+                            </a>
+                        </div>
+                        `
+                },
+            },
         ]
     });
 }

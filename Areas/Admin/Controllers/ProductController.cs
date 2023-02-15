704a1a8e0c2ff4b10f9e8f2847058a0f6b9d8f09
Giuseppe-lore <loregiuseppe0@gmail.com>
2023-02-15 10:17:27 +0100

    Implemented Product upsert w/ SelectListItem

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@ using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBookWeb.Controllers;
 [Area("Admin")]
@@ -31,9 +32,22 @@ namespace BulkyBookWeb.Controllers;
 
         public IActionResult Upsert(int? id)
         {
-        Product product = new();
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u=> new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
 
-            if(id == null || id == 0)
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+            u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+
+        if (id == null || id == 0)
             {
                 // Create product
                 return View(product);

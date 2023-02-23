994748fd1920309464f39084bb0f75e9c2310883
Giuseppe-lore <loregiuseppe0@gmail.com>
2023-02-23 09:58:12 +0100

    Implemented logic on Status filter in order list

--- Areas/Admin/Controllers/OrderController.cs
+++ Areas/Admin/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
@@ -26,12 +28,30 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         #region API CALLS
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
             IEnumerable<OrderHeader> orderHeaders;
 
             // Retrieve a list of all products and includes category and covertype
             orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u=>u.PaymentStaus==SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStaus == SD.StatusInProcess);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStaus == SD.StatusApproved);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStaus == SD.StatusShipped);
+                    break;
+                default:
+                    break;
+            }
 
             // Return a Json containting the list
             return Json(new { data = orderHeaders });

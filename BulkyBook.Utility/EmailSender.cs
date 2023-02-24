fc18b2084e926df39e9519c990870e3cee9a1a41
Giuseppe-lore <loregiuseppe0@gmail.com>
2023-02-24 10:19:01 +0100

    Implemented Email Sender using MimeKit and MailKit

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace BulkyBook.Utility
 {
@@ -7,6 +9,27 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+
+            emailToSend.From.Add(MailboxAddress.Parse("hello@mail.com"));
+
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage};
+
+            // Send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+
+                emailClient.Authenticate("instantgods@gmail.com", "unstabl34");
+
+                emailClient.Send(emailToSend);
+
+                emailClient.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
